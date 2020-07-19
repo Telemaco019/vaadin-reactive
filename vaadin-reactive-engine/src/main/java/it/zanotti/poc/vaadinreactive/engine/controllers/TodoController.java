2536efa7793ae35cde7d3dc4bc13aab7e704f200
Michele Zanotti <m.zanotti@myti.it>
2020-07-19 15:35:00 +0200

    Fix todo flux mapping

--- vaadin-reactive-engine/src/main/java/it/zanotti/poc/vaadinreactive/engine/controllers/TodoController.java
+++ vaadin-reactive-engine/src/main/java/it/zanotti/poc/vaadinreactive/engine/controllers/TodoController.java
@@ -4,6 +4,7 @@ import it.zanotti.poc.vaadinreactive.core.model.Todo;
 import it.zanotti.poc.vaadinreactive.core.services.TodoService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -26,7 +27,7 @@ public class TodoController {
         this.todoService = todoService;
     }
 
-    @GetMapping("/api/reactive/todos")
+    @GetMapping(value = "/api/reactive/todos", produces = MediaType.APPLICATION_STREAM_JSON_VALUE)
     public Flux<Todo> getTodosReactive() {
         return todoService.getTodosFlux();
     }
