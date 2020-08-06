437f4b37ed400354aab266dc71b84c1cf370d6f2
Michele Zanotti <m.zanotti@myti.it>
2020-08-06 08:53:44 +0200

    Renaming, move @EnableR2dbcRepositories to VaadinReactiveEngineApplication

--- vaadin-reactive-engine/src/main/java/it/zanotti/poc/vaadinreactive/engine/VaadinReactiveEngineApplication.java
+++ vaadin-reactive-engine/src/main/java/it/zanotti/poc/vaadinreactive/engine/VaadinReactiveEngineApplication.java
@@ -2,7 +2,9 @@ package it.zanotti.poc.vaadinreactive.engine;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.r2dbc.repository.config.EnableR2dbcRepositories;
 
+@EnableR2dbcRepositories("it.zanotti.poc.vaadinreactive.engine.db.repositories")
 @SpringBootApplication
 public class VaadinReactiveEngineApplication {
 
--- vaadin-reactive-engine/src/main/java/it/zanotti/poc/vaadinreactive/engine/config/R2dbcConfig.java
+++ vaadin-reactive-engine/src/main/java/it/zanotti/poc/vaadinreactive/engine/config/R2dbcConfig.java
@@ -7,18 +7,16 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.r2dbc.config.AbstractR2dbcConfiguration;
-import org.springframework.data.r2dbc.repository.config.EnableR2dbcRepositories;
 
 /**
  * @author Michele Zanotti on 02/08/20
  **/
 @Configuration
-@EnableR2dbcRepositories("it.zanotti.poc.vaadinreactive.engine.db.repositories")
-public class AppConfig extends AbstractR2dbcConfiguration {
+public class R2dbcConfig extends AbstractR2dbcConfiguration {
     private final R2DBCConfigProps configProps;
 
     @Autowired
-    public AppConfig(R2DBCConfigProps configProps) {
+    public R2dbcConfig(R2DBCConfigProps configProps) {
         this.configProps = configProps;
     }
 
