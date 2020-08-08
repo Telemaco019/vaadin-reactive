b451196f9b1406e88d69c0d52ab7aa038ff9b8ab
Michele Zanotti <m.zanotti@myti.it>
2020-08-08 14:50:19 +0200

    Add constructors

--- vaadin-reactive-engine/src/main/java/it/zanotti/poc/vaadinreactive/engine/db/dto/TodoDto.java
+++ vaadin-reactive-engine/src/main/java/it/zanotti/poc/vaadinreactive/engine/db/dto/TodoDto.java
@@ -1,11 +1,12 @@
 package it.zanotti.poc.vaadinreactive.engine.db.dto;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.relational.core.mapping.Column;
 import org.springframework.data.relational.core.mapping.Table;
 
-import java.time.LocalDate;
 import java.time.LocalDateTime;
 
 /**
@@ -13,6 +14,8 @@ import java.time.LocalDateTime;
  **/
 @Data
 @Table("TODO")
+@NoArgsConstructor
+@AllArgsConstructor
 public class TodoDto {
     @Id
     @Column("ID")
