b0cb8824cc495f43f3d5915cf119d8794c092912
Michele Zanotti <m.zanotti@myti.it>
2020-08-08 14:50:06 +0200

    Add findAll test

--- vaadin-reactive-engine/src/test/java/it/zanotti/poc/vaadinreactive/engine/db/repositories/TodoRepositoryIntegrationTest.java
+++ vaadin-reactive-engine/src/test/java/it/zanotti/poc/vaadinreactive/engine/db/repositories/TodoRepositoryIntegrationTest.java
@@ -1,12 +1,16 @@
 package it.zanotti.poc.vaadinreactive.engine.db.repositories;
 
 import it.zanotti.poc.vaadinreactive.engine.VaadinReactiveEngineApplication;
+import it.zanotti.poc.vaadinreactive.engine.db.dto.TodoDto;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringRunner;
+import reactor.test.StepVerifier;
+
+import java.time.LocalDateTime;
 
 /**
  * @author Michele Zanotti on 02/08/20
@@ -19,7 +23,17 @@ public class TodoRepositoryIntegrationTest {
     private TodoRepository todoRepository;
 
     @Test
-    public void test() {
-        todoRepository.findAll().subscribe(todo -> todo.toString());
+    public void findAll() {
+        LocalDateTime date = LocalDateTime.of(2020, 8, 19, 0, 0);
+        final TodoDto firstExpected = new TodoDto(1, date, "first");
+        final TodoDto secondExpected = new TodoDto(2, date, "second");
+        final TodoDto thirdExpected = new TodoDto(3, date, "third");
+
+        todoRepository.findAll()
+                .as(StepVerifier::create)
+                .expectNext(firstExpected)
+                .expectNext(secondExpected)
+                .expectNext(thirdExpected)
+                .verifyComplete();
     }
 }
