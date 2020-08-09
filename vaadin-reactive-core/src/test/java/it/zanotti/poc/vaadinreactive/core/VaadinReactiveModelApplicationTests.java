ec8a0e367b28dbdc8a48291f7b6c3f5232c0ef69
Michele Zanotti <m.zanotti@myti.it>
2020-08-09 11:22:16 +0200

    Remove unused test class

--- vaadin-reactive-core/src/test/java/it/zanotti/poc/vaadinreactive/core/VaadinReactiveModelApplicationTests.java
+++ vaadin-reactive-core/src/test/java/it/zanotti/poc/vaadinreactive/core/VaadinReactiveModelApplicationTests.java
@@ -1,12 +1,4 @@
 package it.zanotti.poc.vaadinreactive.core;
 
-import org.junit.Assert;
-import org.junit.Test;
-
 public class VaadinReactiveModelApplicationTests {
-
-    @Test
-    public void test() {
-        Assert.assertEquals("", "");
-    }
 }
